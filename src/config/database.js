8d5bfb6e01b0fa8860e12d370a2e41227cfaff94
oyugi <oyugimaurice22@gmail.com>
2025-06-05 23:58:41 +0300

    inntegrate  neon postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,20 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : [],
-});
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  const pool = new Pool({ connectionString: process.env.NEON_POOLED_URL });
+  const adapter = new PrismaNeon(pool);
+  prisma = new PrismaClient({ adapter });
+} else {
+  if (!global.prisma) {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaNeon(pool);
+    global.prisma = new PrismaClient({ adapter });
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
